a2fff83913335792b6e2681785a63bd38c910177
Daniel Weber <daniel.weber@uni-tuebingen.de>
2019-09-24 15:53:42 +0200

    Initial commit

---
+++ hri_gaze_mapping/GazeMapper.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+
+
+class GazeMapper:
+    def __init__(self, ref_filename, videoCap):
+        self.aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_250)
+        self.parameters = cv2.aruco.DetectorParameters_create()
+        self.parameters.markerBorderBits = 2
+        self.parameters.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
+
+        self.ref = cv2.imread(ref_filename, cv2.IMREAD_ANYCOLOR)
+        self.corners, self.ids, _ = self.detect(self.ref)
+
+        self.ids = self.ids.flatten()
+        self.refDimensions = self.ref.shape
+        self.corners = np.asarray(self.corners).reshape(4*len(self.ids), 2)
+        self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
+        self.out_field = cv2.VideoWriter('gaze_out.mp4', self.fourcc, 30,
+                                         (int(videoCap.get(cv2.CAP_PROP_FRAME_WIDTH)),
+                                          int(videoCap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
+        self.out_ref = cv2.VideoWriter('gaze_out_ref.mp4', self.fourcc, 30, (int(self.refDimensions[1]),
+                                                                                   int(self.refDimensions[0])))
+        self.out_ref_nogaze = cv2.VideoWriter('no_gaze_out_ref.mp4', self.fourcc, 30,
+                                              (int(self.refDimensions[1]), int(self.refDimensions[0])))
+
+        assert len(self.ids) >= 4, 'Reference image must contain at least four markers'
+
+    def detect(self, img):
+        if np.shape(img)[2] == 3:
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        else:
+            gray = img
+        return cv2.aruco.detectMarkers(gray, self.aruco_dict, parameters=self.parameters)
+
+    def release(self):
+        self.out_field.release()
+        self.out_ref.release()
+        self.out_ref_nogaze.release()
+
+    def map(self, img, gazepoint):
+        def show(name, img, gp, radius):
+            tmp = img.copy()
+            cv2.circle(tmp, (gp[0], gp[1]), radius, (0, 255, 0), 5)
+            tmp2 = cv2.resize(tmp, None, fx=0.4, fy=0.4)
+            cv2.imshow(name, tmp2)
+            self.out_field.write(tmp)
+
+        show("field", img, gazepoint, 15)
+
+        corners, ids, _ = self.detect(img)
+
+        if np.all(ids is not None):
+            ids = ids.flatten()
+            corners = np.asarray(corners).reshape(4*len(ids), 2)
+            matching_ids = np.intersect1d(self.ids, ids)
+            if len(matching_ids) > 0:
+                src = []
+                dst = []
+
+                def addCorners(matching_id, ids_list, corners_list, matched_corners_list):
+                    idx = np.nonzero(matching_id == ids_list)[0][0]
+                    for corner in corners_list[4*idx:4*idx+4, :]:
+                        matched_corners_list.append(corner)
+
+                for matching_id in matching_ids:
+                    addCorners(matching_id, ids, corners, src)  # from current frame
+                    addCorners(matching_id, self.ids, self.corners, dst)  # to reference
+
+                assert len(src) == len(dst)
+
+                src = np.float32(src)
+                dst = np.float32(dst)
+
+                h, mask = cv2.findHomography(src, dst)
+
+                warped_nogaze = cv2.warpPerspective(img, h, (self.ref.shape[1], self.ref.shape[0]))
+                self.out_ref_nogaze.write(warped_nogaze)
+                # cv2.imshow("no gaze", warped_nogaze)
+
+                # Robot camera
+                cv2.circle(img, (gazepoint[0], gazepoint[1]), 15, (0, 255, 0), 5)
+                warped_gaze = cv2.warpPerspective(img, h, (self.ref.shape[1], self.ref.shape[0]))
+                warped_preview = cv2.resize(warped_gaze, None, fx=0.1, fy=0.1)
+                cv2.imshow("ref", warped_preview)
+                self.out_ref.write(warped_gaze)
+
+                # in stimulus coordinate
+                # src = np.float32([[[gazepoint[0], gazepoint[1]]]])
+                # dst = cv2.perspectiveTransform(src, h)
+                # show("stimulus", self.ref, dst[0][0], 30)
---
+++ hri_gaze_mapping/visualize_gaze.py
@@ -0,0 +1,78 @@
+import csv
+import argparse
+import cv2
+import numpy as np
+
+from GazeMapper import GazeMapper
+
+
+def readTimestamps(filename):
+    with open(filename, newline='\n') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        return [float(row['timestamp']) for row in reader]
+
+
+def readJournal(filename):
+    with open(filename, newline='\n') as file:
+        reader = csv.DictReader(file, delimiter='\t')
+        return [row for row in reader]
+
+
+def match(timestamps, source, key_name='timestamp'):
+    data = [{'timestamp': ts, 'matches': []} for ts in timestamps]
+
+    current = 0
+    lastIdx = len(data)-1
+    for row in source:
+        ts = float(row[key_name])
+
+        # find first entry equal or larger than ts
+        while ts > data[current]['timestamp'] and current < lastIdx:
+            current += 1
+
+        timediff = lambda idx: abs(ts - data[idx]['timestamp'])
+
+        # Is current or prev closer to ts?
+        prev = max(0, current-1)
+        matchedIdx = prev if timediff(prev) < timediff(current) else current
+        data[matchedIdx]['matches'].append(row)
+
+    return data
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Overlay gaze on top of field video and map gaze on robot view')
+
+    parser.add_argument('fieldVideo', action='store')
+    parser.add_argument('fieldVideoTimestamps', action='store')
+    parser.add_argument('journal', action='store')
+    args = parser.parse_args()
+    timestamps = readTimestamps(args.fieldVideoTimestamps)
+    journal = readJournal(args.journal)
+    matched = match(timestamps, journal, 'sync.timestamp')
+    video = cv2.VideoCapture(args.fieldVideo)
+
+    # Check if total number of frames equals number of timestamps
+    assert video.get(cv2.CAP_PROP_FRAME_COUNT) == len(timestamps)
+
+    mapper = GazeMapper('robot_view.jpg', video)
+
+    while True:
+        ret, frame = video.read()
+        if not ret:
+            break
+
+        cur_frame_idx = int(video.get(cv2.CAP_PROP_POS_FRAMES)) - 1
+
+        get = lambda name: int(round(np.median([float(entry[name]) for entry in matched[cur_frame_idx]['matches']])))
+        x = get('field.gaze.x')
+        y = get('field.gaze.y')
+
+        mapper.map(frame, (x, y))
+        key = cv2.waitKey(30) & 0xFF
+
+        if key == ord('q'):
+            break
+
+    mapper.release()
