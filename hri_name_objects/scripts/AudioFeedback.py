f5d104d6c679ed9be41c7e18d52956061073fbfb
Daniel Weber <daniel.weber@uni-tuebingen.de>
2021-02-03 14:46:06 +0100

    Adds fullscreen toggle and deletes gTTS AudioFeedback

--- hri_name_objects/scripts/AudioFeedback.py
+++ hri_name_objects/scripts/AudioFeedback.py
@@ -1,6 +1,3 @@
-import os
-import pygame
-from gtts import gTTS
 import pyttsx3
 
 
@@ -17,35 +14,3 @@ class AudioFeedback:
     def say(self, text):
         self.engine.say(text)
         self.engine.runAndWait()
-
-
-class AudioFeedbackGTTS:
-
-    def __init__(self, audio_folder_path="."):
-        self.audio_folder_path = audio_folder_path
-        self.pg = pygame
-        self.pg.mixer.pre_init(44100, -16, 2, 4096)
-        self.pg.mixer.init(44100, -16, 2, 4096)
-        self.pg.init()
-        self.language = "en"
-
-    def __del__(self):
-        self.pg.mixer.quit()
-
-    def play(self, mp3_filename):
-        self.pg.mixer.music.load(self.audio_folder_path + mp3_filename)
-        self.pg.mixer.music.play(0)
-
-    def stop(self):
-        self.pg.mixer.stop()
-
-    def say(self, text):
-        file_name = "voice.mp3"
-        tts = gTTS(text, lang=self.language)
-        tts.save(self.audio_folder_path + file_name)
-        self.play(file_name)
-        while self.pg.mixer.get_busy():
-            print(self.pg.mixer.get_busy())
-            pygame.time.wait(100)
-        os.remove(self.audio_folder_path + file_name)
-
--- hri_name_objects/scripts/OnScreenDisplay.py
+++ hri_name_objects/scripts/OnScreenDisplay.py
@@ -1,31 +1,45 @@
 import pygame
+import sys
 
 
 class OnScreenDisplay:
 
-    def __init__(self, welcome_message='', fullscreen=True):
+    def __init__(self, welcome_message='', fullscreen=False):
         self.pg = pygame
         self.pg.init()
+        self.display_dimensions = self._get_display_dimensions()
         self.fullscreen = fullscreen
         self.font = pygame.font.SysFont("ubuntu", 27)
         self.color_text = (0, 185, 0)
         self.color_background = (255, 255, 255)
+        self.current_text = welcome_message
         self._initialize_screen(welcome_message)
-
-    def _initialize_screen(self, welcome_message):
-        if self.fullscreen is True:
-            self.screen = pygame.display.set_mode(self._get_display_dimensions(), pygame.FULLSCREEN)
-        else:
-            self.screen = pygame.display.set_mode((640, 480))
-        self.screen_rect = self.screen.get_rect()
-        self.change_text(welcome_message)
 
     def _get_display_dimensions(self):
         return pygame.display.Info().current_w, pygame.display.Info().current_h
+
+    def _set_display_mode(self):
+        if self.fullscreen is True:
+            self.screen = pygame.display.set_mode(self.display_dimensions, pygame.FULLSCREEN)
+        else:
+            self.screen = pygame.display.set_mode((640, 480))
+        self.screen_rect = self.screen.get_rect()
+
+    def _initialize_screen(self, welcome_message):
+        self._set_display_mode()
+        self.change_text(welcome_message)
+
+        clock = pygame.time.Clock()  # for limiting FPS
+        while True:
+            for event in self.pg.event.get():
+                if event.type == self.pg.KEYDOWN and event.key == self.pg.K_f:
+                    self.fullscreen = not self.fullscreen
+                    self._set_display_mode()
+                    self.change_text(self.current_text)
+            clock.tick(60)
 
     def change_text(self, new_text):
         text = self.font.render(new_text, True, self.color_text)
         self.screen.fill(self.color_background)
         self.screen.blit(text, text.get_rect(center=self.screen_rect.center))
-        # self.screen.blit(text, (320 - text.get_width() // 2, 240 - text.get_height() // 2))
         self.pg.display.flip()
